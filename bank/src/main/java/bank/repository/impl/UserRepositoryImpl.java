f2ed87747ad8f583c06087dbd340acffb7e08aec
Olena <olenapatsevko@gmail.com>
2020-01-26 00:21:05 +0200

    login register problem solved

--- bank/src/main/java/bank/repository/impl/UserRepositoryImpl.java
+++ bank/src/main/java/bank/repository/impl/UserRepositoryImpl.java
@@ -17,7 +17,7 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void save(User entity) {
-        userMap.replace(entity.getId(), entity);
+        userMap.put(entity.getId(), entity);
     }
 
     @Override
